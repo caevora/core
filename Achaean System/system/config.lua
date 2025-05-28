d71ef3514fc524082524f9d5826f2f1ec0b2f326
Caevora <caevorasmailbox@gmail.com>
2025-05-28 06:46:35 -0600

    🎉 Initial commit

--- Achaean System/system/config.lua
+++
@@ -1,6 +0,0 @@
---CONFIGURATION FILE
-mpackage = [[Achaean System]]
-author = [[Shane Rasmussen (shanesrasmussen@gmail.com)]]
-icon = [[Round_Button_01.png]]
-version = [[V_2.0.2]]
-created = "2024-05-14T22:01:48-07:00"
