dc14fa23945399790a7b86dab6a9c31d2468e47b
caevora <caevorasmailbox@gmail.com>
2025-05-28 04:28:36 -0600

    Update config.lua
    Updated version file for testing.

--- Achaean System/system/config.lua
+++ Achaean System/system/config.lua
@@ -2,5 +2,5 @@
 mpackage = [[Achaean System]]
 author = [[Shane Rasmussen (shanesrasmussen@gmail.com)]]
 icon = [[Round_Button_01.png]]
-version = [[V_2.0.0]]
+version = [[V_2.0.2]]
 created = "2024-05-14T22:01:48-07:00"
