14c6a42f3b0d0b751a4d4ace346cd41f99deaba9
Caevora <caevorasmailbox@gmail.com>
2025-06-19 22:14:54 -0600

    ? Auto-backup: Profile snapshot

--- Achaean System/config.lua
+++ Achaean System/config.lua
@@ -2,9 +2,5 @@ mpackage = [[Achaean System]]
 author = [[Shane Rasmussen (shanesrasmussen@gmail.com)]]
 icon = [[Round_Button_01.png]]
 description = [[A fully functional system for Achaea]]
-<<<<<<< HEAD
-version = [[V_2.0.78]]
-=======
-version = [[V_2.0.79]]
->>>>>>> bc65330 (? Auto-backup: Profile snapshot)
+version = [[V_2.0.80]]
 created = "2025-01-01T11:21:18-08:00"
