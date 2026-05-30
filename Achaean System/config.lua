92327e6e08de5761dd11a444da125e5b3e43d62e
User <caevorasmailbox@gmail.com>
2026-05-29 21:28:31 -0700

    ? Auto-backup: Profile snapshot

--- Achaean System/config.lua
+++ Achaean System/config.lua
@@ -3,5 +3,5 @@ author = [[Shane Rasmussen (shanesrasmussen@gmail.com)]]
 icon = [[Round_Button_01.png]]
 title = [[A fully functional system for Achaea]]
 description = [[A fully functional curing & utility system for Achaea]]
-version = [[2.0.154]]
+version = [[2.0.155]]
 created = "2025-06-20T15:22:23-06:00"
